43f55883d887734f264fddaf3e28b8293f4cee8d
Sliking <miguelpinto25@hotmail.com>
2016-05-24 15:16:52 +0100

    Add Hasher class

---
+++ src/main/java/com/web/crawler/Hasher.java
@@ -0,0 +1,23 @@
+package com.web.crawler;
+
+import java.security.MessageDigest;
+
+public class Hasher {
+	public static String toSha256(String string){
+		try{
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			String text = string.toLowerCase();
+			md.update(text.getBytes("ASCII"));
+			byte[] hash = md.digest();
+			
+			StringBuilder sb = new StringBuilder();
+			for(byte b : hash){
+				sb.append(String.format("%02x", b));
+			}
+			return "";
+		}catch(Exception e){
+			return e.toString();
+		}
+		
+	}
+}
